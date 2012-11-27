b410ac849d0c2a7684b6f643f56d23796b75fa73
Mauricio Quezada <waxkun@gmail.com>
2012-11-27 08:06:42 -0300

    [dataset]

--- v2/dataset.py
+++ v2/dataset.py
@@ -1,19 +1,30 @@
 import events_crawler
 import events_enricher
 import documents_getter
+from time import time
 
 
 def main():
+    tt0 = time()
     # step 1
+    t0 = time()
     events_crawler.get_news()
-    events_crawler.get_festivals()
+    print "time:", str((time() - t0))
 
+    t0 = time()
+    events_crawler.get_festivals()
+    print "time:", str((time() - t0))
+
+    t0 = time()
     # step 2
     events_enricher.enrich_events()
+    print "time:", str((time() - t0))
 
+    t0 = time()
     # step 3
     documents_getter.generate_documents()
+    print "time:", str((time() - t0))
 
-
+    print "total: ", str((time() - tt0))
 if __name__ == '__main__':
     main()
